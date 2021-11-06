6caf5fb6e6c1f314f928d8e4e04cbc244a909554
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-11-06 20:52:49 +0100

    async

--- useeffect-api-2/useeffect-app/src/App.js
+++ useeffect-api-2/useeffect-app/src/App.js
@@ -3,10 +3,30 @@ import React, {useState, useEffect} from 'react';
 
 const App = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    /* fetch('https://jsonplaceholder.typicode.com/posts')
+    .then((response) => response.json())
+    .then((data) => setData(data.splice(0, 10))); */
+    (async () => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      const data =await response.json();
+      setTimeout(() => {
+        setData(data.splice(0, 10))
+        setIsLoading(false)
+      },3000)
+
+    })();
+  },[])
   return (
 
-    <div className="App">
-      asd
+    <div>
+      {isLoading ? <div>Loading </div>: 
+      <ul>
+        {data.map((item) => {
+          return <li key={item.id}>{item.body}</li>
+        })}
+      </ul>}
     </div>
   );
 }
