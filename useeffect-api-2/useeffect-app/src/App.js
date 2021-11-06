7544cf8de841e33fda3e9b089411b1b48c0237ac
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-11-06 20:25:34 +0100

    useeffect-api

---
+++ useeffect-api-2/useeffect-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+
+const App = () => {
+  const [data, setData] = useState([]);
+  return (
+
+    <div className="App">
+      asd
+    </div>
+  );
+}
+
+export default App;
