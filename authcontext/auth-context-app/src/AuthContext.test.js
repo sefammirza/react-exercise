f734d11657e6cf93f072a62cf32533d63ac2c610
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-11-16 22:17:57 +0100

    authprovider

--- authcontext/auth-context-app/src/AuthContext.test.js
+++ authcontext/auth-context-app/src/AuthContext.test.js
@@ -2,4 +2,28 @@ import React from "react";
 import { AuthContext, AuthProvider } from "./AuthContext";
 import {mount} from 'enzyme'
 
-describe("")
+describe("login", () => {
+    it("sets LoggedIn status to true",() => {
+        const TestComponent = () => {
+           const {login, isLoggedIn} = React.useContext(AuthContext)
+           
+           return <>
+           
+           <div data-testid="value">{isLoggedIn.toString()}</div>
+           <button onClick={login}>Login</button>
+
+           </>
+        }
+
+        const wrapper = mount(<AuthProvider>
+            <TestComponent/>
+        </AuthProvider>);
+
+        expect(wrapper.find('[data-testid="value"]').text()).toEqual("false")
+
+        wrapper.find('button').simulate('click')
+
+
+        expect(wrapper.find('[data-testid="value"]').text()).toEqual("true")
+    })
+})
