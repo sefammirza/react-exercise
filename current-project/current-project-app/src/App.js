3d60f29b09f883596540e22b90f0afca6aafa764
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-11-20 20:20:48 +0100

    counter

--- current-project/current-project-app/src/App.js
+++ current-project/current-project-app/src/App.js
@@ -1,18 +1,31 @@
 import React, {useState} from 'react'
 
-class App extends React.Component {
 
+function countInitial () {
+  console.log('run function')
+  return 4
 }
 
 function App() {
 
-  const [count, setCount] = useState(4)
+  const [count, setCount] = useState((countInitial) => {
+    console.log('run function')
+    return 4
+  })
+
+  function decrementCount(){
+    setCount(prevCount => prevCount-1 )
+  }
+
+  function incrementCount () {
+    setCount(prevCount => prevCount + 1)
+  }
 
   return (
     <>
-    <button>-</button>
-    <span>0</span>
-    <button>+</button>
+    <button onClick={decrementCount}>-</button>
+    <span>{count}</span>
+    <button onClick={incrementCount}>+</button>
     </>
   );
 }
