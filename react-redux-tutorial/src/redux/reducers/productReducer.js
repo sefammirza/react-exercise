442aa9f734273e45fa59c4b19a1f7d8917dff4bd
sefammirza <81354116+sefammirza@users.noreply.github.com>
2021-12-14 22:43:18 +0100

    productdetails

--- react-redux-tutorial/src/redux/reducers/productReducer.js
+++ react-redux-tutorial/src/redux/reducers/productReducer.js
@@ -12,8 +12,8 @@ const initialState = {
 
 
 
-export const productReducer = (state , {type, payload}) => {
-    switch (action.type) {
+export const productReducer = (state = initialState , {type, payload}) => {
+    switch (type) {
         case ActionsTypes.SET_PRODUCTS:
             return state;
     
