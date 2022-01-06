a81cd56434f4b54464b7d894d78aad3f10a7e4fd
sefammirza <81354116+sefammirza@users.noreply.github.com>
2022-01-06 10:50:45 +0100

    redux2

---
+++ redux-2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppRouter from './router';
+
+
+
+
+const App = () => {
+    return (
+        <div>
+            <AppRouter />
+        </div>
+    )
+}
+
+export default App
